6270ecf4f1992c6f8cc538e8baff1e2197f5705b
tripathinn8 <30956659+tripathinn8@users.noreply.github.com>
2017-08-14 20:45:42 +0530

    Add files via upload

---
+++ Associate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Associate  {
+
+	public final double salary=100;
+	public final void leave()
+	{
+		System.out.println("Leaves are mandatory for employees");
+	}
+}
---
+++ Associate1.java
@@ -0,0 +1,13 @@
+
+public class Associate1 extends Associate
+{
+	
+
+	public static void main(String[] args) 
+	{
+		Associate1 asc =new Associate1();
+		asc.leave();//cannot override this method so called by object
+
+	}
+
+}
